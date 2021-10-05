a3c383c6c95f434c2629709d7cb3e7591b6d9a81
Oliver Hargreaves <77515872+OliverHargreaves@users.noreply.github.com>
2021-10-04 19:14:08 -0600

    Update Summary.101.R
    Added cumulative ET.
    Minor improvements.

--- Summary.101.R
+++ Summary.101.R
@@ -8,105 +8,128 @@ library(hrbrthemes)
 library(multcompView)
 
 # Load data ####
-data=read_excel("Results.L_Edited.xlsx") # load the SM data from excel
-data=data[1:15]
+data=read_excel("Results.101.xlsx") # load the SM data from excel
+data[is.na(data)]=0
 
 # Plot the results ####
 
 # Method A
-plot(data$Date, data$ET.A.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method A - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.A.L)))
+plot(data$Date, data$ET.A, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method A', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.A.L, 
+plot(data$Date, data$Kc.A, 
      type='h', lwd=3, col='orchid3',
-     main='Method A - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.A.L)))
+     main='Kc - Method A', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
 
 # Method B
-plot(data$Date, data$ET.B.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method B - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.B.L)))
+plot(data$Date, data$ET.B, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method B', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.B.L, 
+plot(data$Date, data$Kc.B, 
      type='h', lwd=3, col='orchid3',
-     main='Method B - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.B.L)))
+     main='Kc - Method B', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
 
 # Method C
-plot(data$Date, data$ET.C.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method B - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.C.L)))
+plot(data$Date, data$ET.C, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method C', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.C.L, 
+plot(data$Date, data$Kc.C, 
      type='h', lwd=3, col='orchid3',
-     main='Method C - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.C.L)))
+     main='Kc - Method C', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
 
 # Method D
-plot(data$Date, data$ET.D.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method D - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.D.L)))
+plot(data$Date, data$ET.D, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method D', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.D.L, 
+plot(data$Date, data$Kc.D, 
      type='h', lwd=3, col='orchid3',
-     main='Method D - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.D.L)))
+     main='Kc - Method D', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
 
 # Method E
-plot(data$Date, data$ET.E.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method E - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.E.L)))
+plot(data$Date, data$ET.E, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method E', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.E.L, 
+plot(data$Date, data$Kc.E, 
      type='h', lwd=3, col='orchid3',
-     main='Method E - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.E.L)))
+     main='Kc - Method E', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
 
 # Method F
-plot(data$Date, data$ET.F.L, 
-     type='h', lwd=3, col='darkslategray3',
-     main='Method F - Left side', xlab='', ylab='ET (mm)',
-     ylim=c(0, max(data$ET.F.L)))
+plot(data$Date, data$ET.F, 
+     type='h', lwd=3, col='darkturquoise',
+     main='ET - Method F', xlab='', ylab='ET (mm)',
+     ylim=c(0, 12))
 lines(data$Date, data$ETo,
       lwd=3, col='darkseagreen3')
+legend('topright', legend=c('ETo', 'ET'), col=c('palegreen3', 'darkturquoise'), lwd=3, inset=0.02)
 
-plot(data$Date, data$Kc.F.L, 
+plot(data$Date, data$Kc.F, 
      type='h', lwd=3, col='orchid3',
-     main='Method F - Left side', xlab='', ylab='ET/ETo',
-     ylim=c(0, max(data$Kc.F.L)))
+     main='Kc - Method F', xlab='', ylab='ET/ETo',
+     ylim=c(0, 1.3))
+
+# Cumulative ET time series ####
+ET.A.cum=cumsum(data$ET.A)
+ET.B.cum=cumsum(data$ET.B)
+ET.C.cum=cumsum(data$ET.C)
+ET.D.cum=cumsum(data$ET.D)
+ET.E.cum=cumsum(data$ET.E)
+ET.F.cum=cumsum(data$ET.F)
+
+plot(data$Date, ET.A.cum, type='l', lwd=2, col='aquamarine', main='Cumulative ET', xlab='', ylab='ET (mm)')
+lines(data$Date, ET.B.cum, type='l', lwd=2, col='chocolate')
+lines(data$Date, ET.C.cum, type='l', lwd=2, col='chartreuse')
+lines(data$Date, ET.D.cum, type='l', lwd=2, col='goldenrod')
+lines(data$Date, ET.E.cum, type='l', lwd=2, col='brown1')
+lines(data$Date, ET.F.cum, type='l', lwd=2, col='deeppink')
+legend('topleft', legend=c(paste('Method', LETTERS[seq(from=1, to=6)])), col=c('aquamarine', 'chocolate', 'chartreuse', 'goldenrod', 'brown1', 'deeppink'), lwd=3, inset=0.02)  
+
 
 # Calculate summary statistics ####
 summ=summary(data[4:15])
 
 ETo.sum=sum(data$ETo)
-ET.A.L.sum=sum(data$ET.A.L)
-ET.B.L.sum=sum(data$ET.B.L)
-ET.C.L.sum=sum(data$ET.C.L)
-ET.D.L.sum=sum(data$ET.D.L)
-ET.E.L.sum=sum(data$ET.E.L)
-ET.F.L.sum=sum(data$ET.F.L)
+ET.A.sum=sum(data$ET.A)
+ET.B.sum=sum(data$ET.B)
+ET.C.sum=sum(data$ET.C)
+ET.D.sum=sum(data$ET.D)
+ET.E.sum=sum(data$ET.E)
+ET.F.sum=sum(data$ET.F)
 Method=c('A','B','C','D','E','F')
-sum.df=data.frame(Method, ET.Season=c(ET.A.L.sum, ET.B.L.sum, ET.C.L.sum, ET.D.L.sum, ET.E.L.sum, ET.F.L.sum))
+sum.df=data.frame(Method, ET.Season=c(ET.A.sum, ET.B.sum, ET.C.sum, ET.D.sum, ET.E.sum, ET.F.sum))
 
 plot(1:6, sum.df$ET.Season, 
      main='Total ET for the season', xlab='Method', ylab='ET (mm)', ylim=c(0, max(sum.df$ET.Season)), xaxt='n',
-     type='h', lwd=8, col='darkslategray3')
+     type='h', lwd=8, col='darkturquoise')
 axis(1, at=1:6, labels=Method)
 
 abline(h=ETo.sum, col='darkseagreen3', lwd=8)
@@ -115,12 +138,12 @@ abline(h=ETo.sum, col='darkseagreen3', lwd=8)
 # Box plots ####
 n=length(data$Date)
 method=rep(c('A','B', 'C','D','E', 'F'), each=n)
-ET.L=c(data$ET.A.L, data$ET.B.L, data$ET.C.L, data$ET.D.L, data$ET.E.L, data$ET.F.L) # Evapotranspiration
-Kc.L=c(data$Kc.A.L, data$Kc.B.L, data$Kc.C.L, data$Kc.D.L, data$Kc.E.L, data$Kc.F.L) # Evapotranspiration
-Left.df=data.frame(date=data$Date, method, ET.L, Kc.L)
+ET=c(data$ET.A, data$ET.B, data$ET.C, data$ET.D, data$ET.E, data$ET.F) # Evapotranspiration
+Kc=c(data$Kc.A, data$Kc.B, data$Kc.C, data$Kc.D, data$Kc.E, data$Kc.F) # Evapotranspiration
+Left.df=data.frame(date=data$Date, method, ET, Kc)
   
 # ET 
-ggplot(Left.df, aes(x = method, y = ET.L, fill = date)) +
+ggplot(Left.df, aes(x = method, y = ET, fill = date)) +
   geom_boxplot() +
   scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
   theme_ipsum() +
@@ -128,12 +151,12 @@ ggplot(Left.df, aes(x = method, y = ET.L, fill = date)) +
     legend.position="none",
     plot.title = element_text(size=11)
   ) +
-  ggtitle("Daily ET method comparison - left side") +
+  ggtitle("Daily ET method comparison") +
   xlab("Methods")+ylab("ET (mm)") + 
-  geom_boxplot(color="black", fill="darkslategray3")
+  geom_boxplot(color="black", fill="darkturquoise")
 
 # Kc 
-ggplot(Left.df, aes(x = method, y = Kc.L, fill = date)) +
+ggplot(Left.df, aes(x = method, y = Kc, fill = date)) +
   geom_boxplot() +
   scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
   theme_ipsum() +
@@ -141,14 +164,14 @@ ggplot(Left.df, aes(x = method, y = Kc.L, fill = date)) +
     legend.position="none",
     plot.title = element_text(size=11)
   ) +
-  ggtitle("Daily Kc method comparison - left side") +
+  ggtitle("Daily Kc method comparison") +
   xlab("Methods")+ylab("ET/ETo") + 
   geom_boxplot(color="black", fill="orchid3")
 
 # Violin plots ####
 
 # ET 
-ggplot(Left.df, aes(x = method, y = ET.L, fill = date)) +
+ggplot(Left.df, aes(x = method, y = ET, fill = date)) +
   geom_violin() +
   scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
   theme_ipsum() +
@@ -156,12 +179,12 @@ ggplot(Left.df, aes(x = method, y = ET.L, fill = date)) +
     legend.position="none",
     plot.title = element_text(size=11)
   ) +
-  ggtitle("Daily ET method comparison - left side") +
+  ggtitle("Daily ET method comparison") +
   xlab("Methods")+ylab("ET (mm)") + 
-  geom_violin(color="black", fill="darkslategray3")
+  geom_violin(color="black", fill="darkturquoise")
 
 # Kc 
-ggplot(Left.df, aes(x = method, y = Kc.L, fill = date)) +
+ggplot(Left.df, aes(x = method, y = Kc, fill = date)) +
   geom_violin() +
   scale_fill_viridis(discrete = TRUE, alpha=0.6, option="A") +
   theme_ipsum() +
@@ -169,28 +192,28 @@ ggplot(Left.df, aes(x = method, y = Kc.L, fill = date)) +
     legend.position="none",
     plot.title = element_text(size=11)
   ) +
-  ggtitle("Daily Kc method comparison - left side") +
+  ggtitle("Daily Kc method comparison") +
   xlab("Methods")+ylab("ET/ETo") + 
   geom_violin(color="black", fill="orchid3")
 
 # ANOVA + Tukey test ####
 
 # ET
-model=lm( Left.df$ET.L ~ Left.df$method )
+model=lm( Left.df$ET ~ Left.df$method )
 ANOVA=aov(model)
 
 # Tukey test to study each pair of treatment :
-TUKEY <- TukeyHSD(x=ANOVA, 'Left.df$method', conf.level=0.95)
+TUKEY <- TukeyHSD(x=ANOVA, 'Left.df$method', confevel=0.95)
 
 # Tuckey test representation :
 plot(TUKEY , las=1 , col="brown")
 
 # Kc
-model=lm(Left.df$Kc.L ~ Left.df$method)
+model=lm(Left.df$Kc ~ Left.df$method)
 ANOVA=aov(model)
 
 # Tukey test to study each pair of treatment :
-TUKEY <- TukeyHSD(x=ANOVA, 'Left.df$method', conf.level=0.95)
+TUKEY <- TukeyHSD(x=ANOVA, 'Left.df$method', confevel=0.95)
 
 # Tuckey test representation :
 plot(TUKEY , las=1 , col="brown")
